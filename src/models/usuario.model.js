6cc817faf353a6cc0d0666a609545f67ce3daf31
walt13xd <walt13xd@gmail.com>
2024-02-22 15:01:25 -0300

    Terminada implementação de bcrypt para senha

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -3,9 +3,10 @@ import {Schema, model} from 'mongoose'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-const SCRECT = process.env.TOKEN_SECRET
+const TOKEN_SECRET = process.env.TOKEN_SECRET
 
 const usuarioSchema = new Schema({
     email: String,
@@ -15,25 +16,29 @@ const usuarioSchema = new Schema({
 const Usuario = model('Usuario', usuarioSchema)
 
 const getAll = async () => {
-    return Usuario.find({}, { senha: 0 }) // Exclui o campo 'senha' do resultado
+    return await Usuario.find({}, { senha: 0 }) // Exclui o campo 'senha' do resultado
 }
 
 //bcrypt senha
 const encryptSenha = async (senha) => {
-    const salt = await bcrypt.genSalt(12);
-    const senhaHash = await bcrypt.hash(senha, salt);
-    return senhaHash
+    const salt = await bcrypt.genSalt(12)
+    return await bcrypt.hash(senha, salt)
 }
 
 //novo usuário
 const newUser = async ({ email, senha }) => {
-    
-    const senhaHash = encryptSenha(senha)
-
-    return Usuario.create({ email, senhaHash })
+    const senhaHash = await encryptSenha(senha)
+    return Usuario.create({ email, senha: senhaHash })
 }
 
-
+const findUserByEmail = async (email) => {
+    return await Usuario.findOne({ email })
+}
+/*
+const findUserById = async (id) => {
+    return await Usuario.findById(id);
+}
+*/
 const userExists = async ({ email, id }) => {
     if (id) {
         return Usuario.findById(id)
@@ -47,43 +52,45 @@ const deleta = async ({ id }) => {
 }
 
 const update = async ({ id, email, senha }) => {
-    
-    const senhaHash = encryptSenha(senha)
-
-    return Usuario.findByIdAndUpdate(id, { email, senhaHash })
+    const senhaHash = await encryptSenha(senha)
+    return Usuario.findByIdAndUpdate(id, { email, senhaHash })    
 }
 
 const login = async ({ email, senha }) => {
-    
-    const senhaHash = encryptSenha(senha)
-    const user = await Usuario.findOne({ email })
-    const checkedPassword = bcrypt.compare(senhaHash, user.senha);
-    
-    if(checkedPassword){
-        return user
-    }else{
-        throw new Error('Uuário ou senha incorreto')
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            throw new Error('Usuário não encontrado');
+        }
+        const isPasswordValid = bcrypt.compare(senha, user.senha);
+        if (isPasswordValid) {
+            return user;
+        } else {
+            throw new Error('Usuário ou senha incorretos');
+        }
+    } catch (error) {
+        throw error;
     }
 }
 
 const requestLogin = async (req, res) => {
-    const { email, senha } = req.body
-    const usuario = await login({ email, senha })
-
-    if (!usuario) return res.status(401).json({ message: 'Usuário não encontrado' })
-
-    const token = jwt.sign(
-        {
-            userId: usuario._id,
-            email,
-        },
-        SCRECT,
-        {
-            expiresIn: 1440,
-        },
-    );
-    return res.status(201).json({ token })
+    const { email, senha } = req.body;
+    try {
+        const usuario = await login({ email, senha });
+        const token = jwt.sign(
+            {
+                userId: usuario._id,
+                email,
+            },
+            TOKEN_SECRET,
+            {
+                expiresIn: 1440,
+            }
+        );
+        res.status(201).json({ token });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
 }
 
 export { getAll, newUser, userExists, deleta, update, requestLogin }
-
